97561150830e3d8f5fe22d71572cf8e82465daf2
Kevin Serina <kevinserina392@gmail.com>
2022-06-30 11:43:56 -0400

    Complete fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,7 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+	if (Number(num) < 0) return "OOPS";
+	if (Number(num) <= 1) return Number(num);
+	return fibonacci(num - 1) + fibonacci(num - 2);
 };
 
 // Do not edit below this line
